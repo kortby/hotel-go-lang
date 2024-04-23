5d474249cbd42a0a34e15d4ee59aac7574998b0b
kortby@gmail.com <benk@Bens-MBP.lan>
2024-04-22 21:07:02 -0700

    validation

--- types/user.go
+++ types/user.go
@@ -1,12 +1,17 @@
 package types
 
 import (
+	"fmt"
+	"regexp"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
 
 const (
-	bcryptCost = 12 // Good practice to make this configurable based on environment
+	bcryptCost = 12
+	minNameLen = 2
+	minPasswordLen = 7
 )
 
 type CreateUserParams struct {
@@ -22,6 +27,32 @@ type User struct {
 	LastName           string             `bson:"lastName" json:"lastName"`
 	Email              string             `bson:"email" json:"email"`
 	EncryptedPassword  string             `bson:"encryptedPassword" json:"-"`
+}
+
+
+// emailRegex is a regular expression for validating email addresses.
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+// validateEmail checks if the email provided matches the RFC 5322 standard.
+func validateEmail(email string) bool {
+	return emailRegex.MatchString(email)
+}
+
+func (params CreateUserParams) Validate() []string {
+	errors := []string{}
+	if len(params.FirstName) < minNameLen {
+		errors = append(errors, fmt.Sprintf("first name length should be at leas %d characters", minNameLen))
+	}
+	if len(params.LastName) < minNameLen {
+		errors = append(errors, fmt.Sprintf("last name length should be at leas %d characters", minNameLen))
+	}
+	if len(params.Password) < minPasswordLen {
+		errors = append(errors, fmt.Sprintf("password length should be at leas %d characters", minPasswordLen))
+	}
+	if !validateEmail(params.Email) {
+		errors = append(errors, fmt.Sprintln("please return a valid email"))
+	}
+	return errors
 }
 
 func NewUserFromParams(params CreateUserParams) (*User, error) {
